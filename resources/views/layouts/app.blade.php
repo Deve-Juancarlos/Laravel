68e8678a3f7473f6fd9b4a3b91c5a004af352549
Sofnx32 <juan.ochoa@tecsup.edu.pe>
2025-10-27 20:25:55 -0500

    Actualizar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -151,6 +151,8 @@
     
     <!-- SweetAlert2 -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 
     <script src="{{ asset('js/app.js') }}"></script>
 
